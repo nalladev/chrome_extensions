4eb742154730b43aaaf94d7139d6d2cbff7ce760
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2024-01-31 00:47:42 +0530

    focus tub feature addition, bug fix

---
+++ focus_mode_youtube/inject.js
@@ -0,0 +1,45 @@
+const progressElementName = "yt-page-navigation-progress";
+const searchElementName = "search_query";
+const className = "focus-tube-home";
+
+function handlePathChange() {
+  const path = window.location.pathname;
+  if (path == "/") {
+    document.body.classList.add(className);
+  } else {
+    document.body.classList.remove(className);
+  }
+}
+
+function observeMutations(progressElement) {
+  const observer = new MutationObserver(() => {
+    if (progressElement.hidden) handlePathChange();
+  });
+  observer.observe(progressElement, { attributes: true, subtree: false });
+}
+
+function handlePageLoad() {
+  const progressElement = document.getElementsByTagName(progressElementName)[0];
+  if (!progressElement) {
+    setTimeout(handlePageLoad, 500);
+    return;
+  }
+  observeMutations(progressElement);
+}
+
+function handleSearchLoad() {
+  const searchElement = document.getElementsByName(searchElementName)[0];
+  if (!searchElement) {
+    setTimeout(handleSearchLoad, 500);
+    return;
+  }
+  searchElement.focus();
+}
+
+function init() {
+  handlePathChange();
+  handleSearchLoad();
+  handlePageLoad();
+}
+
+init();
