f0ffad6ad5f20a081b6e709c995d9958a710feca
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2024-07-21 20:17:35 +0530

    button insert logic addition

--- clickbait/inject.js
+++ clickbait/inject.js
@@ -6,6 +6,8 @@ function newTagElement() {
 }
 
 const reportButton = document.createElement("button");
+reportButton.classList.add("clickbait-btn");
+reportButton.textContent = "CB";
 
 function checkClickbait(videoId) {
   return true;
@@ -13,44 +15,48 @@ function checkClickbait(videoId) {
 
 function handleAddedNodes(nodes) {
   nodes.forEach((node) => {
+    if (!node.tagName) return;
     if (
-      node.href &&
+      node.tagName.toLowerCase() == "a" &&
       (node.href.includes("/watch?v=") || node.href.includes("/shorts/")) &&
       node.classList.contains("ytd-thumbnail")
     ) {
       const videoId = node.href.match(/.+\/(?:watch\?v=)?(.+)/)[1];
       const isClickbait = checkClickbait(videoId);
       if (isClickbait) node.parentNode.appendChild(newTagElement());
-      console.log("added tag to", videoId);
-    } else if (node.id == "actions-inner" && node.childNodes[0].id == "menu") {
-      node.childNodes[0].childNodes[0].appendChild(newButton());
+    } else if (
+      window.location.pathname == "/watch" &&
+      node.tagName.toLowerCase() == "like-button-view-model" &&
+      node.clientHeight != 0
+    ) {
+      console.log(reportButton);
+      document.querySelector(".YtSegmentedLikeDislikeButtonViewModelSegmentedButtonsWrapper").appendChild(reportButton);
     }
   });
 }
 
 const observer = new MutationObserver((mutationsList) => {
-  for (let mutation of mutationsList) {
+  for (const mutation of mutationsList) {
     if (mutation.type === "childList") {
       handleAddedNodes(Array.from(mutation.addedNodes));
     }
   }
 });
 
-function waitForBodyLoad() {
+function waitForContentLoad() {
   return new Promise((resolve) => {
     const intervalId = setInterval(() => {
-      if (document.body) {
+      if (document.getElementById("content")) {
         clearInterval(intervalId);
-        resolve(document.body);
+        resolve(document.getElementById("content"));
       }
     }, 200);
   });
 }
 
 async function init() {
-  const targetNode = await waitForBodyLoad();
-  const config = { childList: true, subtree: true };
-  observer.observe(targetNode, config);
+  const targetNode = await waitForContentLoad();
+  observer.observe(targetNode, { childList: true, subtree: true });
 }
 
 init();
