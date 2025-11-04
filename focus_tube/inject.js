df05416715d90f9e2fbbe349d7f619541ad9b909
Joel <122166595+nalladev@users.noreply.github.com>
2025-11-05 01:48:05 +0530

    keep shorts as it is

--- focus_tube/inject.js
+++ focus_tube/inject.js
@@ -1,70 +1,70 @@
-const searchElementName = "search_query";
-const className = "focus-tube-home";
-const loadTime = 200;
-let focusSearch = () => {};
+// const searchElementName = "search_query";
+// const className = "focus-tube-home";
+// const loadTime = 200;
+// let focusSearch = () => {};
 
-function handleShortsLoad() {
-  const shortElements = document.querySelectorAll("ytd-reel-video-renderer");
-  if (shortElements.length == 0) {
-    setTimeout(handleShortsLoad, loadTime);
-    return;
-  }
-  shortElements.forEach((element) => {
-    if (element.id != 0) element.remove();
-  });
-}
+// function handleShortsLoad() {
+//   const shortElements = document.querySelectorAll("ytd-reel-video-renderer");
+//   if (shortElements.length == 0) {
+//     setTimeout(handleShortsLoad, loadTime);
+//     return;
+//   }
+//   shortElements.forEach((element) => {
+//     if (element.id != 0) element.remove();
+//   });
+// }
 
-function handlePathChange() {
-  if (window.location.pathname == "/") {
-    document.body.classList.add(className);
-    focusSearch();
-  } else document.body.classList.remove(className);
+// function handlePathChange() {
+//   if (window.location.pathname == "/") {
+//     document.body.classList.add(className);
+//     focusSearch();
+//   } else document.body.classList.remove(className);
 
-  if (window.location.pathname.startsWith("/shorts")) {
-    handleShortsLoad();
-  }
-}
+//   if (window.location.pathname.startsWith("/shorts")) {
+//     handleShortsLoad();
+//   }
+// }
 
-function observeMutations(element) {
-  const observer = new MutationObserver(handlePathChange);
-  observer.observe(element, {
-    childList: true,
-  });
-}
+// function observeMutations(element) {
+//   const observer = new MutationObserver(handlePathChange);
+//   observer.observe(element, {
+//     childList: true,
+//   });
+// }
 
-function handleTitleLoad() {
-  const titleElement = document.getElementsByTagName("title")[0];
-  if (!titleElement) {
-    setTimeout(handleTitleLoad, loadTime);
-    return;
-  }
-  observeMutations(titleElement);
-}
+// function handleTitleLoad() {
+//   const titleElement = document.getElementsByTagName("title")[0];
+//   if (!titleElement) {
+//     setTimeout(handleTitleLoad, loadTime);
+//     return;
+//   }
+//   observeMutations(titleElement);
+// }
 
-function handleBodyLoad() {
-  const bodyElement = document.body;
-  if (!bodyElement) {
-    setTimeout(handleBodyLoad, loadTime);
-    return;
-  }
-  handlePathChange();
-}
+// function handleBodyLoad() {
+//   const bodyElement = document.body;
+//   if (!bodyElement) {
+//     setTimeout(handleBodyLoad, loadTime);
+//     return;
+//   }
+//   handlePathChange();
+// }
 
-function handleSearchLoad() {
-  const searchElement = document.getElementsByName(searchElementName)[0];
-  if (!searchElement) {
-    setTimeout(handleSearchLoad, loadTime);
-    return;
-  }
-  focusSearch = () => searchElement.focus();
-  focusSearch();
-}
+// function handleSearchLoad() {
+//   const searchElement = document.getElementsByName(searchElementName)[0];
+//   if (!searchElement) {
+//     setTimeout(handleSearchLoad, loadTime);
+//     return;
+//   }
+//   focusSearch = () => searchElement.focus();
+//   focusSearch();
+// }
 
-function init() {
-  window.addEventListener("popstate", handlePathChange);
-  handleTitleLoad();
-  handleBodyLoad();
-  handleSearchLoad();
-}
+// function init() {
+//   window.addEventListener("popstate", handlePathChange);
+//   handleTitleLoad();
+//   handleBodyLoad();
+//   handleSearchLoad();
+// }
 
-init();
+// init();
